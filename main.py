0d69a7c1ce8cf84b9a3a6d3f6c6bb0a9fe4dd122
jerry <jerry@sina.com>
2023-04-20 17:59:58 +0800

    '第十一个变量'

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    print('Hello, I\'m tom')
+    print('Hello, I\'m jerry')
     num1 = 10
     num2 = 20
     num3 = 30
@@ -23,6 +23,8 @@ if __name__ == '__main__':
     num8 = 80
     num9 = 90
     num10 = 100
+    num11 = 101
+
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
